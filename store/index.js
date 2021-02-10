5be91de5d103f1f348742591641e93430e2b4c5f
Czy <651525974@qq.com>
2021-02-10 22:19:33 +0800

    INIT

---
+++ store/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import {data} from "@/modules/global-data";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: data,
+    getters: {},
+    mutations: {
+        setInitData: function(state, payload) {
+            state.point = payload.tips;
+            state.curWeek = payload.curWeek;
+            state.curTerm = payload.curTerm;
+            state.initData = payload.initData;
+            state.curTermStart = payload.termStart;
+        },
+        setUserStatus: function(state, payload) {
+            state.openid = payload.openid;
+            state.userFlag = payload.status === 1 ? 1 : 0;
+        },
+        setColorList: function(state, payload){
+            state.colorN = payload.colorN;
+            state.colorList = payload.colorList;
+        }
+    },
+    actions: {}
+})
+
+export default store;
---
+++ vector/pub-fct.js
@@ -0,0 +1,19 @@
+import util from "@/modules/datetime";
+
+/**
+ * 统一处理课表功能
+ */
+function tableDispose(info, oneDay = false) {
+    
+}
+
+function getCurWeek(startTime) {
+    console.log(util.formatDate());
+    if (util.formatDate() < startTime) return 1;
+    var week = (util.dayDiff(startTime, util.formatDate()) / 7) >> 0 + 1;
+    return week;
+}
+
+export {getCurWeek, tableDispose}
+
+export default {getCurWeek, tableDispose}
