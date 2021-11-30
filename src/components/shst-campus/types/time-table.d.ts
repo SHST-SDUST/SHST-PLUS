89ffc8b9bea40d54e2988940c618a10b0d83cdbd
Czy <651525974@qq.com>
2021-11-30 22:29:33 +0800

    修改types

--- src/components/shst-campus/types/time-table.d.ts
+++ src/components/shst-campus/types/time-table.d.ts
@@ -1,8 +1,8 @@
 export interface TableItem {
-    className: string;
-    classRoom: string;
-    teacher: string;
-    background: string;
+    className: string; // 课程名
+    classRoom: string; // 教室名
+    teacher: string; // 教师名
+    background: string; // 背景颜色
 }
 export type Tables = Array<
     Array<{
@@ -11,9 +11,9 @@ export type Tables = Array<
     }>
 >;
 export type DefinedTableItem = TableItem & {
-    week: number;
-    serial: number;
-    ext?: string;
-    background?: string;
-    curWeek?: boolean;
+    week: number; // 周几的课
+    serial: number; // 第几节 `5`节制
+    ext?: string; // 扩展信息
+    background?: string; // 背景颜色 无则自动计算
+    curWeek?: boolean; // 若为该周的课 则优先显示
 };
